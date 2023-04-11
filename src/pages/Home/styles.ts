2e2d4337857cf20ef7e026db5852f1b9c78e6deb
claytonfortunato <claytonreis10@gmail.com>
2023-04-11 14:19:32 -0300

    feat: add font

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,36 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.main`
+  min-height: 100vh;
+  padding: 11rem 1.6rem 1.6rem;
 
-export const Header = styled.div``;
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media (min-width: 416px) {
+    max-width: 416px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 832px) {
+    flex-grow: 1;
+  }
+`;
+
+export const Header = styled.div`
+  height: 5.4rem;
+
+  position: absolute;
+  top: 4rem;
+  left: 1.6rem;
+  right: 1.6rem;
+
+  background-color: ${(props) => props.theme.colors.gray50};
+
+  img {
+    height: 100%;
+  }
+`;
